e1b13fd044d541b8effb2d46b89020f98485111c
muxuefeng <muxuefeng@139.com>
2019-02-27 18:08:48 +0800

    put text on video

--- VideoTest/VideoTest/CameraSource.cpp
+++ VideoTest/VideoTest/CameraSource.cpp
@@ -22,7 +22,7 @@ string getTime()
 	time_t now_time;
 	time(&now_time);
 	char tmp[64];
-	strftime(tmp, sizeof(tmp), "%Y%m%d-%H%M%S", localtime(&now_time));
+	strftime(tmp, sizeof(tmp), "%Y.%m.%d %H:%M:%S", localtime(&now_time));
 	return tmp;
 }
 
@@ -56,6 +56,20 @@ int main(int, char**)
 	string filename = "./live.avi";
 	writer.open(filename, codec_MP42, fps, src.size(), isColor);
 
+
+	int font_face = cv::FONT_HERSHEY_SIMPLEX;
+	double font_scale = 1;
+	int thickness = 2;
+	int baseline;
+	cv::Size text_size = cv::getTextSize(getTime(), font_face, font_scale, thickness, &baseline);
+
+	cv::Point origin;
+	origin.x = src.cols / 2 - text_size.width / 2;
+	origin.y = src.rows / 2 + text_size.height / 2;
+
+	origin.x = 0;
+	origin.y = 30;
+
 	if (!writer.isOpened())
 	{
 		cerr << "could not open output video file";
@@ -71,6 +85,9 @@ int main(int, char**)
 			cerr << "error, blank frame";
 			break;
 		}
+
+		cv::putText(src, getTime(), origin, font_face, font_scale, cv::Scalar(0, 255, 255), thickness, 8, 0);
+
 		writer.write(src);
 		imshow("Live", src);
 		if(waitKey(5) >=0 )
